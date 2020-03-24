5fa50ce6d27f943523e2827721af33d7e2ad033a
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-03-24 12:53:52 -0300

    Hotfix(back-end, get('/')): Added project's version to this endpoint

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,10 +7,13 @@ const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
+const version = process.env.VERSION || '^1.0.0';
+
 routes.get('/', (req, res) => {
   res.json({
     App: 'Be the hero\'s back-end API',
-    status: 'Development',
+    Status: 'Development',
+    Version: `${version}`,
     Author: 'Ricardo Morato Rocha'
   });
 });
